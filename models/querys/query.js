042cfaa73160af858d9e2e2e8ac4a79fe339ce2b
evielma <evielma@memorialtechnologies.com>
2024-01-02 17:41:13 -0500

    se agregaron funcioones al query

--- models/querys/query.js
+++ models/querys/query.js
@@ -14,7 +14,7 @@ class Query{
         EstadoFirmador,
         MensajeProceso,
         EstadoComprobanteId,
-        FechaCreacion,
+        CONVERT(varchar,FechaCreacion,20) as [YYYY-MM-DD],
         FechaActualizacion,
         (CASE EstadoEmail WHEN 0 THEN 'PENDIENTE' WHEN 1 THEN 'ENVIADO' WHEN 2 THEN 'ERROR' END) as 'Estado_Envío_Email',
         FechaNotificacion as 'Fecha_Envío_Email',
@@ -39,7 +39,7 @@ class Query{
         EstadoFirmador,
         MensajeProceso,
         EstadoComprobanteId,
-        FechaCreacion,
+        CONVERT(varchar,FechaCreacion,20) as [YYYY-MM-DD],
         FechaActualizacion,
         (CASE EstadoEmail WHEN 0 THEN 'PENDIENTE' WHEN 1 THEN 'ENVIADO' WHEN 2 THEN 'ERROR' END) as 'Estado_Envío_Email',
         FechaNotificacion as 'Fecha_Envío_Email',
@@ -65,7 +65,7 @@ class Query{
         (CASE TipoDocumento WHEN 1 THEN 'FACTURA' WHEN 4 THEN 'NOTA DE CRÉDITO' WHEN 6 THEN 'GUÍA DE REMISIÓN' WHEN 7 THEN 'COMPROBANTE DE RETENCIÓN' WHEN 3 THEN 'LIQUIDACION COMPRA' END) as 'TipoDocumento',
         SUBSTRING(ClaveAcceso,25,15) as Num_Doc,
         EstadoFirmador,
-        FechaCreacion,
+        CONVERT(varchar,FechaCreacion,20) as [YYYY-MM-DD],
         documentoid
         from SafiBDDParametros_pruebas..Fac_Comprobante_Historial 
 		inner join V1791297954001_SAFI_3_pruebas..CXCDIR on SafiBDDParametros_pruebas..Fac_Comprobante_Historial.PersonaId = V1791297954001_SAFI_3_pruebas..CXCDIR.CodigoID
