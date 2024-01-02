dd9cb344ba7db50bcf1af4899c412f558b357a83
evielma <evielma@memorialtechnologies.com>
2024-01-02 17:57:56 -0500

    se agregaron funcioones al query

--- models/querys/query.js
+++ models/querys/query.js
@@ -14,7 +14,7 @@ class Query{
         EstadoFirmador,
         MensajeProceso,
         EstadoComprobanteId,
-        FORMAT(FechaAutorizacion, 'yyyy-MM-dd') AS FechaAutorizacion,
+        FORMAT(FechaCreacion, 'yyyy-MM-dd') AS FechaCreacion,
         FechaActualizacion,
         (CASE EstadoEmail WHEN 0 THEN 'PENDIENTE' WHEN 1 THEN 'ENVIADO' WHEN 2 THEN 'ERROR' END) as 'Estado_Envío_Email',
         FechaNotificacion as 'Fecha_Envío_Email',
@@ -39,7 +39,7 @@ class Query{
         EstadoFirmador,
         MensajeProceso,
         EstadoComprobanteId,
-        FORMAT(FechaAutorizacion, 'yyyy-MM-dd') AS FechaAutorizacion,
+        FORMAT(FechaCreacion, 'yyyy-MM-dd') AS FechaCreacion,
         FechaActualizacion,
         (CASE EstadoEmail WHEN 0 THEN 'PENDIENTE' WHEN 1 THEN 'ENVIADO' WHEN 2 THEN 'ERROR' END) as 'Estado_Envío_Email',
         FechaNotificacion as 'Fecha_Envío_Email',
@@ -53,6 +53,7 @@ class Query{
 
     getInvoiceId(id){
         return `select 
+        documentoid,
 		CXCDIR.Nombre,
 		CXCDIR.Direc1,
 		CXCDIR.Ruc,
@@ -63,7 +64,7 @@ class Query{
         EstadoFirmador,
         NumeroAutorizacion,
         FechaAutorizacion,
-        FORMAT(FechaAutorizacion, 'yyyy-MM-dd') AS FechaAutorizacion,
+        FORMAT(FechaCreacion, 'yyyy-MM-dd') AS FechaCreacion,
         XmlOriginal
         from SafiBDDParametros_pruebas..Fac_Comprobante_Historial 
 		inner join V1791297954001_SAFI_3_pruebas..CXCDIR on SafiBDDParametros_pruebas..Fac_Comprobante_Historial.PersonaId = V1791297954001_SAFI_3_pruebas..CXCDIR.CodigoID
@@ -80,7 +81,7 @@ class Query{
         (CASE TipoDocumento WHEN 1 THEN 'FACTURA' WHEN 4 THEN 'NOTA DE CRÉDITO' WHEN 6 THEN 'GUÍA DE REMISIÓN' WHEN 7 THEN 'COMPROBANTE DE RETENCIÓN' WHEN 3 THEN 'LIQUIDACION COMPRA' END) as 'TipoDocumento',
         SUBSTRING(ClaveAcceso,25,15) as Num_Doc,
         EstadoFirmador,
-        FORMAT(FechaAutorizacion, 'yyyy-MM-dd') AS FechaAutorizacion,
+        FORMAT(FechaCreacion, 'yyyy-MM-dd') AS FechaCreacion,
         documentoid
         from SafiBDDParametros_pruebas..Fac_Comprobante_Historial 
 		inner join V1791297954001_SAFI_3_pruebas..CXCDIR on SafiBDDParametros_pruebas..Fac_Comprobante_Historial.PersonaId = V1791297954001_SAFI_3_pruebas..CXCDIR.CodigoID
@@ -99,7 +100,7 @@ class Query{
         EstadoFirmador,
         NumeroAutorizacion,
         FechaAutorizacion,
-        FORMAT(FechaAutorizacion, 'yyyy-MM-dd') AS FechaAutorizacion,
+        FORMAT(FechaCreacion, 'yyyy-MM-dd') AS FechaCreacion,
         XmlOriginal
         from SafiBDDParametros_pruebas..Fac_Comprobante_Historial 
 		inner join V1791297954001_SAFI_3_pruebas..CXCDIR on SafiBDDParametros_pruebas..Fac_Comprobante_Historial.PersonaId = V1791297954001_SAFI_3_pruebas..CXCDIR.CodigoID
